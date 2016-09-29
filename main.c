258f4625342d11e5c439ef9d23838b0f2dbccac0
Ivo Machado <Ivo Machado>
2016-09-29 16:21:50 -0400

    Sugestão para joliana

--- main.c
+++ main.c
@@ -4,8 +4,9 @@
 int main() {
     int n, m;
     int a, b;
-    int c;
+    int c, maior;
     int i, j, *arestas;
+    //fila de arestas a serem removidas caminhos == maior
     TGrafo *g;
     scanf("%d%d", &n, &m);
     g = criarGrafoAdjacencia(n);
@@ -14,11 +15,18 @@ int main() {
         g->addArestaPonderada(g, a, b, c);
     }
     arestas = g->repeticoesArestasCaminhosMinimos(g);
+    maior = 0;
     for(i = 0; i < n; i++) {
         for(j = 0; j < n; j++) {
-            if(arestas[n*i + j] != 0)
+            if(arestas[n*i + j] > maior)
+                // esvaziar fila
+                //inserir (i+1)->(j+1)
+            else
+                //igual a maior
+                // //inserir (i+1)->(j+1)
                 printf("(%d, %d): %d\n", i + 1, j + 1, arestas[n*i + j]);
         }
     }
+    //para cada aresta na fila, remove-a do grafo
     return 0;
 }
